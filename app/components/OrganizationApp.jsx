578ed1c0aad1c6193a759e0411341dd513153bd9
prasadwa <prasad.wanigasinghe@ebuilder.com>
2017-06-19 12:11:43 +0530

    Organization add modal

--- app/components/OrganizationApp.jsx
+++ app/components/OrganizationApp.jsx
@@ -2,14 +2,49 @@ import React from 'react';
 
 import OrganizationSearch from 'OrganizationSearch';
 import OrganizationList from 'OrganizationList';
+import AddOrganizationModal from './AddOrganizationModal';
+
 
 export default class OrganizationApp extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      showModal:false
+    };
+
+    this._addOrganization = this._addOrganization.bind(this);
+  }
+
+  _addOrganization(){
+    this.setState({
+        showModal:true
+    });
+  }
+
   render () {
     return(
       <div>
-        <h1>Organizations</h1>
-        <OrganizationSearch/>
+        <div className="row">
+          <div className="large-12 columns">
+            <h2>Organizations</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="large-6 columns">
+            <OrganizationSearch/>
+          </div>
+          <div className="large-3 large-offset-3 columns">
+            <button type="button" className="button" onClick={() => this._addOrganization()}>Add</button>
+          </div>
+        </div>
         <OrganizationList/>
+
+        {
+          (this.state.showModal)?<AddOrganizationModal />:null
+        }
+
       </div>
     );
   }
