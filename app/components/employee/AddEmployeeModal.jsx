0ce061d66cffdd077b00a23ee66d9c088ee10e67
supungamage <supungamage@gmail.com>
2017-07-13 18:59:47 +0530

    salary details

--- app/components/employee/AddEmployeeModal.jsx
+++ app/components/employee/AddEmployeeModal.jsx
@@ -47,6 +47,7 @@ class AddEmployeeModal extends React.Component{
     e.preventDefault();
     var {dispatch} = this.props;
     var empId = this.refs.empID.value;
+    var basicSalary = this.refs.basic.value;
 
     if(empId !== "") {
 
@@ -60,6 +61,10 @@ class AddEmployeeModal extends React.Component{
         "empRole":"developer",
         "bankAccountNumber":"123321123321",
         "subscribed":true
+      }
+
+      if(basicSalary && basicSalary.length > 0) {
+        // add salary and save
       }
 
       dispatch(actions.startAddEmployee(employee));
@@ -81,13 +86,56 @@ class AddEmployeeModal extends React.Component{
           <div>
             <form ref="form" onSubmit={this.handleSubmit.bind(this)}>
               <h1 className="page-title">Create Employee</h1>
-              <label>Employee ID :</label><input type="text" ref="empID" placeholder="Employee ID"/>
-              <label>First Name :</label><input type="text" ref="empFirstName" placeholder="First Name"/>
-              <label>Last Name :</label><input type="text" ref="empLastName" placeholder="Last Name"/>
-              <label>Organization :</label><input type="text" ref="empOrganization" placeholder="Organization"/>
-              <label>Department :</label><input type="text" ref="empDepartment" placeholder="Department"/>
-              <label>Employee Category :</label><input type="text" ref="empCategory" placeholder="Employee Category"/>
-              <label>Employee Role :</label><input type="text" ref="empRole" placeholder="Employee Role"/>
+              <div>
+                <h3 className="page-title">Basic Details</h3>
+                <label>Employee ID :</label><input type="text" ref="empID" placeholder="Employee ID"/>
+                <label>First Name :</label><input type="text" ref="empFirstName" placeholder="First Name"/>
+                <label>Last Name :</label><input type="text" ref="empLastName" placeholder="Last Name"/>
+                <label>Organization :</label><input type="text" ref="empOrganization" placeholder="Organization"/>
+                <label>Department :</label><input type="text" ref="empDepartment" placeholder="Department"/>
+                <label>Employee Category :</label><input type="text" ref="empCategory" placeholder="Employee Category"/>
+                <label>Employee Role :</label><input type="text" ref="empRole" placeholder="Employee Role"/>
+              </div>
+              <div>
+                <h3 className="page-title">Salary Details</h3>
+                <label>Basic Salary :</label><input type="text" ref="basic" placeholder="Basic Salary"/>
+                <div>
+                  <h3 className="page-title">Additions</h3>
+                  <button type="button" className="button button-margin" >+</button>
+                    <div id="addition_container">
+                      <div class="row" id="addition_1">
+                        <div class="small-12 medium-6 columns">
+                          <label>Name
+                            <input type="text" id="aditionName_1" placeholder="Addition name"/>
+                          </label>
+                        </div>
+                        <div class="small-12 medium-6 columns">
+                          <label>Amount
+                            <input type="text" id="aditionAmount_1" placeholder="Addition amount"/>
+                          </label>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div>
+                  <h3 className="page-title">Deductions</h3>
+                    <button type="button" className="button button-margin" >+</button>
+                    <div id="deduction_container">
+                      <div class="row" id="deduction_1">
+                        <div class="small-12 medium-6 columns">
+                          <label>Name
+                            <input type="text" id="deductionName_1" placeholder="Deduction name"/>
+                          </label>
+                        </div>
+                        <div class="small-12 medium-6 columns">
+                          <label>Amount
+                            <input type="text" id="deductionAmount_1" placeholder="Deduction amount"/>
+                          </label>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
               <button type="submit" className="button button-margin">Create</button>
               <button type="button" className="button button-margin" onClick={this.onClose.bind(this)}>Close</button>
             </form>
